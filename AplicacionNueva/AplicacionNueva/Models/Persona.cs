448683df68937b5d8cc8a226c8a211e3bbd70d29
jean2656 <jean2656@hotmail.com>
2016-11-19 15:20:06 -0600

    pinpie la clase persona, ya esta lista

---
+++ AplicacionNueva/AplicacionNueva/Models/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MagnaDB;
+
+namespace AplicacionNueva.Models
+{
+    public class Persona : TableModel<Persona>
+    {
+        protected override string ConnectionString
+        {
+            get { return MvcApplication.connectionString; }
+        }
+        protected override MagnaKey Key
+        {
+            get { return this.MakeKey(c => c.Id); }
+        }
+        protected override string TableName
+        {
+            get { return "Personas"; }
+        }
+
+        [Identity]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        
+        
+        [DateTimeType(DateTimeSpecification.Date)]
+        [ColumnName("FechaNacimiento")]
+        public DateTime? Fecha { get; set; }
+
+        [DMLIgnore]
+        public int Edad { get; set; }
+    }
+}
